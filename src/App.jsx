187831fc7996804f69de9d9ed768151cf4406773
almamutuguti <alma.wanjiru8j@gmail.com>
2025-08-13 09:44:02 +0300

    setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import BookDetails from "./pages/BookDetails"
+import Dashboard from "./pages/Dashboard"
+import Login from "./pages/Login"
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/book/:id" element={<BookDetails />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
+    </Router>
   )
 }
 
