152b1a4dea8f843cb22290caa4f5dd3c694fc009
almamutuguti <alma.wanjiru8j@gmail.com>
2025-08-13 09:54:25 +0300

    setup Homepage and card display

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { books } from '../services/dummybooks'
+import BookCard from '../components/BookCard'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='p-6'>
+      <h1 className='text-3xl font-bold text-purple-700 mb-4'>Welcome to Talespace</h1>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+          {books.map(book => (
+            <BookCard key={book.id} book={book}/>
+          ))}
+      </div>
+    </div>
   )
 }
 
---
+++ src/services/dummybooks.js
@@ -0,0 +1,19 @@
+export const books = [
+    {
+        id: "1",
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        genre: "Classic",
+        coverUrl: "https://covers.openlibrary.org/b/id/7222246-L.jpg",
+        description: "A novel set in the Jazz Age..."
+    },
+
+    {
+        id: "2",
+        title: "1984",
+        author: "George Orwell",
+        genre: "Dystopian",
+        coverUrl: "https://covers.openlibrary.org/b/id/7222246-L.jpg",
+        description: "A chilling vision of totalitarianism..."
+    }
+]
