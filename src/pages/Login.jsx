4356f0203ad96975d8a4b406682b2f9186f541a4
almamutuguti <alma.wanjiru8j@gmail.com>
2025-08-13 16:26:00 +0300

    create login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useAuth } from '../context/AuthContext'
+
 
 function Login() {
+
+    const { login } = useAuth();
   return (
-    <div>Login</div>
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-50'>
+       <h1 className='text-3xl font-bold text-purple-700 mb-6'>Welcome to Talespace</h1> 
+       <button onClick={login}
+                className='text-white px-6 py-3 rounded hover:bg-purple-700 bg-purple-600'
+        >
+        Sign in with Google
+       </button>
+    </div>
   )
 }
 
