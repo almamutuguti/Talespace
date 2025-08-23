50deece4418e5bacb58efc4ff4bddd29e60f1e3b
almamutuguti <alma.wanjiru8j@gmail.com>
2025-08-24 01:18:32 +0300

    update profile file

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { auth } from '../services/Firebase';
 import { GoogleAuthProvider } from 'firebase/auth';
+import { BookOpen, Mail } from 'lucide-react';
 
 
 
@@ -11,58 +12,132 @@ function Login() {
   const { signUp } = useAuth();
   const [ email, setEmail ] = useState("");
   const [ password, setPassword ] = useState("")
+  const [ showPassword, setShowPassword ] = useState("")
+  const [loading, setLoading] = useState(true)
+  const [error, setError] =useState("")
+  const [ isLogin, setIsLogin ] = useState(true)
   const navigate = useNavigate()
 
-  const handleEmailLogin = async () => {
-    // const result = await emailLogin(email, password)
-    // if (result) navigate("/dashboard")
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('')
+    setLoading(true)
 
     try {
-      await signInWithEmailAndPassword(auth, email, password);
-      navigate('/dashboard')
+      if (isLogin) {
+        await signInWithEmailAndPassword(auth, email, password)
+      } else {
+        await signUp(email, password)
+      }
+
+      navigate("/dashboard")
     } catch (error) {
-      console.error('Login error: ', error.code,  error.message)
+      setError(getErrorMessage(error.code))
+      console.error('Auth Error: ', error.code, error.message)
+    } finally {
+      setLoading(false)
     }
   }
 
+  // const handleEmailLogin = async () => {
+  //   // const result = await emailLogin(email, password)
+  //   // if (result) navigate("/dashboard")
+
+  //   try {
+  //     await signInWithEmailAndPassword(auth, email, password);
+  //     navigate('/dashboard')
+  //   } catch (error) {
+  //     console.error('Login error: ', error.code,  error.message)
+  //   }
+  // }
+
   const handleGoogleLogin = async () => {
+    setError("")
+    setLoading(true)
+
     try {
       const provider = new GoogleAuthProvider()
-      const result = await signInWithPopup(auth, provider)
-      if (result) navigate("/dashboard");
+      await signInWithPopup(auth, provider)
+      navigate("/dashboard");
     } catch (error) {
+      setError(getErrorMessage(error.code))
       console.error('Google login error: ', error.code,  error.message)
+    } finally {
+      setLoading(false)
+    }
+  };
+
+  const getErrorMessage = (errorCode) => {
+    switch (errorCode) {
+      case "auth/invalid-email":
+        return "Invalid email address";
+      case "auth/user-disabled":
+        return "Account disabled";
+      case "auth/user-not-found":
+        return "No account found with this email";
+      case "auth/wrong-password":
+        return "Incorrect password";
+      case "auth/email-already-in-use":
+        return "Email already in use";
+      default:
+        return "Error occurred during authentication";
+      
+      
+      
+ 
     }
   }
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-gray-50'>
-       <h1 className='text-3xl font-bold text-purple-700 mb-6'>Welcome to Talespace</h1>
-       <div className='w-full max-w-sm bg-white p-6 rounded shadow'>
-          <input type="email"
-                  placeholder='Email'
-                  className='w-full mb-2 p-2 border rounded'
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
-          />
-          <input type="password"
-                  placeholder='Password'
-                  className='w-full mb-4 p-2 border rounded'
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-          />
-          <button className='w-full bg-purple-500 text-white py-2 rounded hover:bg-purple-600 mb-2' onClick={handleEmailLogin}>
-            Login
-          </button>
-          <button onClick={() => signUp(email, password)} className='w-full bg-gray-200 py-2 rounded hover:bg-gray-300'>
-            Sign Up
-          </button>
-        </div> 
-       <button onClick={handleGoogleLogin}
-              className='text-white px-6 py-3 rounded hover:bg-purple-700 bg-purple-600'
-        >
-        Sign in with Google
-       </button>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-50 bg-gradient-to-br from-purple-50 to-blue-100 p-4'>
+       <div className='w-full max-w-md'>
+
+        {/* Header */}
+        <div className='text-center mb-8'>
+          <div className='flex items-center justify-center mb-4'>
+            <BookOpen className='w-6 h-6 text-black'/>
+          </div>
+
+          <h1 className='text-4xl font-bold text-gray-900 mb-2'>
+            Talespace
+          </h1>
+
+          <p className='text-gray-600'>
+            Your Journey through stories begins here
+          </p>
+
+        </div>
+
+        {/* Login form */}
+        <div className='bg-white rounded-2xl shadow-xl p-8'>
+          <h2 className='text-2xl font-bold text-center text-gray-900'>
+            {isLogin ? "Welcome back:" : "Create Account"}
+          </h2>
+
+          <div className='mb-4'>
+              {/* Error handling */}
+
+              {error && (
+                <div className=' mt-4 bg-red-50 border border-red-200
+                rounded-lg text-red-700 text-center p-1 text-sm'>
+                  {error}
+                </div>
+              )}
+
+          </div>
+
+          {/* form */}
+          <form onSubmit={handleSubmit} className='space-y-4'>
+            {/* Email */}
+            <div className='relative'>
+              <Mail className='absolute left-3 top-1/2'/>
+
+            </div>
+          </form>
+            
+        </div>
+
+       </div>
     </div>
 
     
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,23 +1,136 @@
 import React, { useEffect, useState } from 'react'
 import { useAuth } from '../context/AuthContext'
 import { getUserStats } from '../services/Firestore';
+import Spinner from '../components/Spinner';
+import { BookOpen, Heart, LogOut, Mail, User } from 'lucide-react';
 
 function Profile() {
     const { user, logout } = useAuth();
     const [stats, setStats] = useState({ read: 0, favorites: 0 })
+    const [loading, setLoading] = useState(true)
+    const [error, setError] =useState("")
 
     useEffect(() => {
-        if (user) {
-            getUserStats(user).then(setStats)
+        // if (user) {
+        //     getUserStats(user).then(setStats)
+        // }
+        const fetchStats = async () => {
+          if (user) {
+            try {
+              setLoading(true);
+              const userStats = await getUserStats(user)
+              setStats(userStats || { read: 0, favorites: 0 })
+            } catch (err) {
+              setError('Failed to load status')
+              console.error("Error fetching status:", err)
+            } finally {
+              setLoading(false)
+            }
+          }
         }
+        fetchStats();
     }, [user])
+
+    const handleLogout = async () => {
+      try {
+        await logout();
+      } catch (error) {
+        console.error('Failed to logout;', error)
+      }
+    }
+
+    if (loading) {
+      return <Spinner />
+    }
   return (
-    <div>
-        <h2>My Profile</h2>
+    <div className='min-h-screen bg-gray-50 flex items-center justify-center'>
+        {/* <h2>My Profile</h2>
         <p><strong>Email:</strong>{user?.email}</p>
         <p><strong>Books Read:</strong>{stats.read}</p>
         <p><strong>Email:</strong>{stats.favorites}</p>
-        <button onClick={logout}>Logout</button>
+        <button onClick={logout}>Logout</button> */}
+        <div className='max-w-4xl mx-auto px-4'>
+          {/* header */}
+          <div className='text-center mb-8'>
+            <h1 className='text-3xl font-bold text-gray-900 mb-2'>My Profile</h1>
+            <p className='text-gray-600'>Manage your reading journey and prefences</p>
+          </div>
+          {/* Profile card */}
+          <div className='bg-white rounded shadow-lg p-6 mb-6'>
+
+            {/* userInfo */} 
+            <div className='flex items-center mb-6'>
+              <div className='w-20 h-20 bg-gradient-to-br from-black to-gray-400 rounded-full flex items-center justify-center mr-4'>
+                  <User className='w-6 h-6 text-white '/>
+              </div>
+              <div>
+                <h2 className='text-xl font-semibold text-gray-900'>
+                  {user?.displayName || "Reader"}
+                </h2>
+                <div className='flex items-center text-gray-600 mt-1'>
+                  <Mail className='w-4 h-4 mr-2'/>
+                  <span>{user?.email}</span>
+                </div>
+              </div>
+            </div>
+
+            {/* status grid */}
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-6'>
+              <div className='bg-blue-50 rounded-xl p-4 text-center'>
+                <div className='flex items-center justify-center mb-2'>
+                  <BookOpen className='w-6 h-6 text-black'/>
+                </div>
+
+                <h3 className='text-2xl font-bold text-gray-600 mb-1'>
+                  {stats.read}
+                </h3>
+                <p className='text-sm text-gary-800'>Books Read</p>
+              </div>
+
+              <div className='bg-gray-100 rounded-xl p-4 text-center'>
+                <div className='flex items-center justify-center mb-2'>
+                  <Heart className='w-6 h-6 text-gray-600'/>
+                </div>
+
+                <h3 className='text-2xl font-bold text-gray-600 mb-1'>
+                  {stats.favorites}
+                </h3>
+                <p className='text-sm text-gary-800'>Favorites</p>
+              </div>
+
+              
+              
+
+            </div>
+
+            <div className='mb-4'>
+              {/* Error handling */}
+
+              {error && (
+                <div className=' mt-4 bg-red-50 border border-red-200
+                rounded-lg text-red-700 text-center p-1 text-sm'>
+                  {error}
+                </div>
+              )}
+
+            </div>
+            
+            <div className='text-center'>
+                  <button onClick={handleLogout} className='
+                  inline-flex items-center px-6 py-3 border border-transparent
+                  text-base font-medium rounded-md text-white bg-red-600
+                  hover:bg-red-700 focus:outline-none focus:ring-2
+                  focus:ring-offset-2 focus:ring-red-500 transition-colors
+                  duration-200'>
+
+                    <LogOut className='w-5 h-5 mr-2'/>
+
+                    Sign Out 
+
+                  </button>
+              </div>
+          </div>
+        </div>
     </div>
   )
 }
